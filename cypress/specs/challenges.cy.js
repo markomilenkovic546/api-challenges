b90985854db9510108f3c3a2ba483bb2b4e7001e
markomilenkovic546 <markomilenkovic546@gmail.com>
2024-03-25 15:19:14 +0100

    challange 4 is done

--- cypress/specs/challenges.cy.js
+++ cypress/specs/challenges.cy.js
@@ -1,4 +1,4 @@
-const tv4 = require("tv4");
+const tv4 = require('tv4');
 
 describe('Challenger session creation', () => {
     it('Should create a session and store a token as an env var', () => {
@@ -14,40 +14,57 @@ describe('Challenger session creation', () => {
         });
     });
 
-    describe('GET Challenges', () => {
-        /* Issue a GET request on the `/challenges` end point*/
-        it('GET /challenges (200)', () => {
-            cy.api({
-                method: 'GET',
-                url: '/challenges',
-                headers: {
-                    'X-Challenger': Cypress.env('X-Challenger')
-                }
-            }).then((response) => {
-                expect(response.body.challenges[0].status).to.eqls(true);
-                expect(response.body.challenges[1].status).to.eqls(true);
+    /* Issue a GET request on the `/challenges` end point*/
+    it('GET /challenges (200)', () => {
+        cy.api({
+            method: 'GET',
+            url: '/challenges',
+            headers: {
+                'X-Challenger': Cypress.env('X-Challenger')
+            }
+        }).then((response) => {
+            expect(response.body.challenges[0].status).to.eqls(true);
+            expect(response.body.challenges[1].status).to.eqls(true);
+        });
+    });
+});
+
+describe('GET Challenges', () => {
+    // Issue a GET request on the `/todos` end point
+    it('GET /todos (200)', () => {
+        cy.api({
+            method: 'GET',
+            url: '/todos',
+            headers: {
+                'X-Challenger': Cypress.env('X-Challenger')
+            }
+        }).then((response) => {
+            expect(response.status).to.eqls(200);
+            cy.fixture('json-schemas/GET todos (200).json').then((schema) => {
+                // Validate the response body against the schema
+                const isValid = tv4.validate(response.body, schema);
+                expect(isValid).to.be.true;
             });
+            // Verify that challenge is complited
+            cy.verifyChallenge(2);
         });
     });
 
-    describe('GET Challenges', () => {
-      // Issue a GET request on the `/todos` end point
-      it('GET /todos (200)', () => {
+    // Issue a GET request on the `/todo` end point should 404 because nouns should be plural
+    it('GET /todo (404) not plural', () => {
         cy.api({
-          method: 'GET',
-          url: '/todos',
-          headers: {
-              'X-Challenger': Cypress.env('X-Challenger')
-          }
+            method: 'GET',
+            url: '/todo',
+            headers: {
+                'X-Challenger': Cypress.env('X-Challenger')
+            },
+            failOnStatusCode: false
         }).then((response) => {
-          expect(response.status).to.eqls(200);
-          cy.fixture("json-schemas/GET todos (200).json").then((schema) => {
-            // Validate the response body against the schema
-            const isValid = tv4.validate(response.body, schema);
-            expect(isValid).to.be.true;
-          });
-        })
-      });
+            expect(response.status).to.eqls(404);
+            // Verify that challenge is complited
+            cy.verifyChallenge(3);
+        });
+    });
 
-    })
+    
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('verifyChallenge', (index) => { 
+    cy.api({
+        method: 'GET',
+        url: '/challenges',
+        headers: {
+            'X-Challenger': Cypress.env('X-Challenger')
+        }
+    }).then((response) => {
+        expect(response.body.challenges[index].status).to.eqls(true);
+        
+    });
+});
+
